87329b2bb8d7a9884758cab04461857c666c3ef7
vishaldhanvijay <vishal.dhanvijay0505@gmail.com>
2026-04-08 20:39:56 +0530

    Added controller and APIs

--- src/main/java/com/journy/deploy/DeployApplication.java
+++ src/main/java/com/journy/deploy/DeployApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class DeployApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeployApplication.class, args);
+		System.out.println("Application Started...");
 	}
 
 }
---
+++ src/main/java/com/journy/deploy/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.journy.deploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "Project is working fine.";
+    }
+}
